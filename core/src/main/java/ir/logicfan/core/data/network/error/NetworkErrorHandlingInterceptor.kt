d9da06b611f563721f7c1571c2bdfffe8b1e3f65
Mahdi <m.javaherisaber@gmail.com>
2019-05-16 14:31:59 +0430

    Fix method signature to add all thrown exceptions

--- core/src/main/java/ir/logicfan/core/data/network/error/NetworkErrorHandlingInterceptor.kt
+++ core/src/main/java/ir/logicfan/core/data/network/error/NetworkErrorHandlingInterceptor.kt
@@ -1,17 +1,17 @@
 package ir.logicfan.core.data.network.error
 
 import android.content.Context
-import ir.logicfan.core.data.network.error.exception.NetworkIOException
 import ir.logicfan.core.data.network.error.exception.NetworkOfflineException
 import ir.logicfan.core.util.Connectivity
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.io.IOException
 import javax.inject.Inject
 
 class NetworkErrorHandlingInterceptor @Inject
 constructor(private val context: Context) : Interceptor {
 
-    @Throws(NetworkIOException::class)
+    @Throws(IOException::class, NetworkOfflineException::class)
     override fun intercept(chain: Interceptor.Chain): Response {
         if (!Connectivity.isUserOnline(context)) {
             throw NetworkOfflineException()
