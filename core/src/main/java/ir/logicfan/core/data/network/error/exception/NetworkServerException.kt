bad50f715804bb09f964aededae5514a41643070
Mahdi <m.javaherisaber@gmail.com>
2019-05-21 18:52:21 +0430

    Fix NetworkServerException class inheritance

--- core/src/main/java/ir/logicfan/core/data/network/error/exception/NetworkServerException.kt
+++ core/src/main/java/ir/logicfan/core/data/network/error/exception/NetworkServerException.kt
@@ -2,13 +2,13 @@ package ir.logicfan.core.data.network.error.exception
 
 import android.content.Context
 import ir.logicfan.core.R
-import ir.logicfan.core.data.error.DataException
 import ir.logicfan.core.data.error.LocalizedError
+import ir.logicfan.core.data.network.error.NetworkException
 
 /**
  * When we have http exception in range 500 - 599 response code
  */
-class NetworkServerException : DataException(), LocalizedError {
+class NetworkServerException : NetworkException(), LocalizedError {
     override fun getLocalizedMessage(context: Context): String {
         return context.resources.getString(R.string.error_server_error)
     }
