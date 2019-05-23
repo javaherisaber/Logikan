52aea56a90565b573436e61e29048baaee8ffbfb
Mahdi <m.javaherisaber@gmail.com>
2019-05-23 18:38:08 +0430

    Add data binding sample adapter, converter and methods

---
+++ core/src/main/java/ir/logicfan/core/util/binding/BindingAdapters.kt
@@ -0,0 +1,29 @@
+package ir.logicfan.core.util.binding
+
+import android.view.View
+import android.widget.ProgressBar
+import androidx.databinding.BindingAdapter
+
+object BindingAdapters {
+
+    /**
+     * hide the view if number is zero
+     */
+    @BindingAdapter("hideIfZero")
+    @JvmStatic
+    fun hideIfZero(view: View, number: Int) {
+        view.visibility = if (number == 0) View.GONE else View.VISIBLE
+    }
+
+    /**
+     *  Sets the value of the progress bar so that 5 likes will fill it up.
+     *
+     *  Showcases Binding Adapters with multiple attributes. Note that this adapter is called
+     *  whenever any of the attribute changes.
+     */
+    @BindingAdapter(value = ["progressScaled", "android:max"], requireAll = true)
+    @JvmStatic
+    fun setProgress(progressBar: ProgressBar, likes: Int, max: Int) {
+        progressBar.progress = (likes * max / 5).coerceAtMost(max)
+    }
+}
---
+++ core/src/main/java/ir/logicfan/core/util/binding/BindingMethods.kt
@@ -0,0 +1,29 @@
+package ir.logicfan.core.util.binding
+
+import android.widget.ImageView
+import androidx.databinding.BindingMethod
+import androidx.databinding.BindingMethods
+
+/**
+ * `app:srcCompat` is an attribute used by the support library to integrate vector drawables. This
+ * BindingMethod binds the attribute to the setImageDrawable method in the ImageView class.
+ *
+ * Binding methods have to be applied to any class in your project. Even an empty one.
+ *
+ * This is equivalent to:
+ * ```
+ *
+ *   @BindingAdapter("srcCompat")
+ *   @JvmStatic fun srcCompat(view: ImageView, @DrawableRes drawableId: Int) {
+ *       view.setImageResource(drawable)
+ *   }
+ * ```
+ */
+@BindingMethods(
+    BindingMethod(
+        type = ImageView::class,
+        attribute = "srcCompat",
+        method = "setImageResource"
+    )
+)
+class MyBindingMethods
