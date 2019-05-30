81bcb1fad4ca37a51d4edf712255428a31a5830d
Mahdi <m.javaherisaber@gmail.com>
2019-05-30 14:57:29 +0430

    Annotate some dagger members with @Reusable scope to make it global
    @Reusable is used for immutable instances and is more prone for
    Garbage collection than @Inject which make's it good citizen in android

--- core/src/main/java/ir/logicfan/core/di/module/BaseModule.kt
+++ core/src/main/java/ir/logicfan/core/di/module/BaseModule.kt
@@ -5,14 +5,17 @@ import android.content.Context
 import androidx.lifecycle.ViewModelProvider
 import dagger.Binds
 import dagger.Module
+import dagger.Reusable
 import ir.logicfan.core.ui.base.BaseViewModelFactory
 
 @Module
 abstract class BaseModule {
 
     @Binds
+    @Reusable
     abstract fun context(application: Application): Context
 
     @Binds
+    @Reusable
     abstract fun baseViewModelFactory(viewModelFactory: BaseViewModelFactory): ViewModelProvider.Factory
 }
