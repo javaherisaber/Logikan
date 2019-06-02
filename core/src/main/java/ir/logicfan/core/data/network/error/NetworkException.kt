067b8dbe71d9f67755a99012b8c11dcb2efaeddb
Mahdi <m.javaherisaber@gmail.com>
2019-06-02 13:53:18 +0430

    Remove redundant exceptions in favor of new strategy using sealed class

--- core/src/main/java/ir/logicfan/core/data/network/error/NetworkException.kt
+++
@@ -1,8 +0,0 @@
-package ir.logicfan.core.data.network.error
-
-import ir.logicfan.core.data.error.DataException
-
-/**
- * Top level exception for network package
- */
-abstract class NetworkException : DataException()
--- core/src/main/java/ir/logicfan/core/ui/error/NetworkConnectivityResolution.kt
+++
@@ -1,6 +0,0 @@
-package ir.logicfan.core.ui.error
-
-interface NetworkConnectivityResolution {
-    fun onConnectivityAvailable(localizedMessage: String?, throwable: Throwable?)
-    fun onConnectivityUnavailable(localizedMessage: String, throwable: Throwable)
-}
