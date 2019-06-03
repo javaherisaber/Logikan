dc34abf9e80e3b3126ccd8bb83e5f14ce86a6433
Mahdi <m.javaherisaber@gmail.com>
2019-06-03 20:17:52 +0430

    Create new adapter and ViewHolder for clickable item layouts

---
+++ core/src/main/java/ir/logicfan/core/ui/adapter/ClickableAdapter.kt
@@ -0,0 +1,32 @@
+package ir.logicfan.core.ui.adapter
+
+import androidx.annotation.LayoutRes
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.MutableLiveData
+import ir.logicfan.core.BR
+import ir.logicfan.core.ui.viewholder.BasicViewHolder
+import ir.logicfan.core.ui.viewholder.ClickableViewHolder
+import ir.logicfan.core.ui.viewholder.ViewHolderListener
+
+/**
+ * Define an adapter which can handle click on root ViewGroup
+ *
+ * @property onViewGroupClickListener callback to handle click on root ViewGroup
+ * @property bindingOnViewGroupClickListenerVariableId variable id inside your view layout
+ */
+class ClickableAdapter<T>(
+    private val onViewGroupClickListener: ViewHolderListener.OnViewGroupClickListener<T>,
+    @LayoutRes itemLayout: Int,
+    private val bindingOnViewGroupClickListenerVariableId: Int = BR.onViewGroupClickListener,
+    bindingVariableId: Int = BR.item,
+    dataSource: MutableLiveData<List<T>> = MutableLiveData()
+) : BasicAdapter<T>(itemLayout, bindingVariableId, dataSource) {
+
+    override fun provideViewHolder(binding: ViewDataBinding): BasicViewHolder<T> =
+        ClickableViewHolder(
+            onViewGroupClickListener,
+            binding,
+            bindingOnViewGroupClickListenerVariableId,
+            bindingDataVariableId
+        )
+}
