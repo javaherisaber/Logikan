bdc3747896aa786dcd587a72871b38391469edec
Mahdi <m.javaherisaber@gmail.com>
2019-06-18 18:58:12 +0430

    Add emitUnitEvent extension to BaseViewHolder

--- core/src/main/java/ir/logicfan/core/ui/base/BaseViewModel.kt
+++ core/src/main/java/ir/logicfan/core/ui/base/BaseViewModel.kt
@@ -1,10 +1,12 @@
 package ir.logicfan.core.ui.base
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import ir.logicfan.core.data.reactive.ErrorStateObserver
+import ir.logicfan.core.ui.reactive.Event
 import ir.logicfan.core.ui.reactive.SingleLiveEvent
 
 abstract class BaseViewModel(val compositeDisposable: CompositeDisposable) : ViewModel(), ErrorStateObserver {
@@ -23,5 +25,11 @@ abstract class BaseViewModel(val compositeDisposable: CompositeDisposable) : Vie
 
     override fun onErrorState(throwable: Throwable) {
         this._errorState.value = throwable
+    }
+
+    companion object {
+        fun emitUnitEvent(liveObject: MutableLiveData<Event<Unit>>) {
+            liveObject.value = Event(Unit)
+        }
     }
 }
