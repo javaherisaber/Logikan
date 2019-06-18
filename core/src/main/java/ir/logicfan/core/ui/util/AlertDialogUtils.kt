217fdc6541193143a9c140b9162f7c54877346bc
Mahdi <m.javaherisaber@gmail.com>
2019-06-18 17:19:44 +0430

    Add go to location screen AlertDialog

--- core/src/main/java/ir/logicfan/core/ui/util/AlertDialogUtils.kt
+++ core/src/main/java/ir/logicfan/core/ui/util/AlertDialogUtils.kt
@@ -1,22 +1,20 @@
 package ir.logicfan.core.ui.util
 
-import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.provider.Settings
+import androidx.annotation.NonNull
+import androidx.appcompat.app.AlertDialog
 
 object AlertDialogUtils {
 
     @JvmStatic
     fun showDialogWithPermissionScreenButton(
-        context: Context,
+        @NonNull context: Context,
         title: String,
         message: String,
-        positiveButtonText: String,
-        negativeButtonText: String,
-        neutralButtonText: String,
-        onNeutralButtonClick: () -> Unit
+        positiveButtonText: String
     ) {
         AlertDialog.Builder(context)
             .setTitle(title)
@@ -25,11 +23,22 @@ object AlertDialogUtils {
                 val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
                 intent.data = Uri.parse("package:" + context.packageName)
                 context.startActivity(intent)
-            }.setNegativeButton(negativeButtonText) { dialog, _ ->
-                dialog.dismiss()
-            }.setNeutralButton(neutralButtonText) { dialog, _ ->
-                dialog.dismiss()
-                onNeutralButtonClick()
+            }.show()
+    }
+
+    @JvmStatic
+    fun showDialogWithLocationScreenButton(
+        @NonNull context: Context,
+        title: String,
+        message: String,
+        positiveButtonText: String
+    ) {
+        AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(positiveButtonText) { _, _ ->
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                context.startActivity(intent)
             }.show()
     }
 }
