441d0e5ca78c5e0e20088daf55ca8b946f936e19
Mahdi <m.javaherisaber@gmail.com>
2019-06-19 07:27:59 +0430

    Remove terminal error handler from BaseActivity (each app has it's own)

--- core/src/main/java/ir/logicfan/core/ui/base/BaseActivity.kt
+++ core/src/main/java/ir/logicfan/core/ui/base/BaseActivity.kt
@@ -1,11 +1,8 @@
 package ir.logicfan.core.ui.base
 
 import android.os.Bundle
-import android.widget.Toast
 import dagger.android.support.DaggerAppCompatActivity
-import ir.logicfan.core.data.error.DataException
 import ir.logicfan.core.data.preferences.BaseSharedPreferences
-import ir.logicfan.core.ui.listener.DataTerminalErrorListener
 import ir.logicfan.core.ui.util.LocaleUtils
 import javax.inject.Inject
 
@@ -14,7 +11,7 @@ import javax.inject.Inject
  * All of our activities extends this class to inherit top level functionality
  */
 
-abstract class BaseActivity : DaggerAppCompatActivity(), DataTerminalErrorListener {
+abstract class BaseActivity : DaggerAppCompatActivity() {
 
     @Inject
     lateinit var baseSharedPreferences: BaseSharedPreferences
@@ -22,15 +19,5 @@ abstract class BaseActivity : DaggerAppCompatActivity(), DataTerminalErrorListen
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LocaleUtils.setLocale(this, baseSharedPreferences.localeSetting)
-    }
-
-    override fun onDataTerminalError(throwable: Throwable) {
-        when (throwable) {
-            is DataException.Terminal.UnAuthorized -> {
-                // logout user info in ActivityBaseViewModel (new class maybe extending from BaseViewModel)
-                // and add login fragment here (also add to back stack)
-                Toast.makeText(this, "please login again", Toast.LENGTH_SHORT).show()
-            }
-        }
     }
 }
