96049d5f56d2add177edf1bd4eb857e4e4d47feb
Mahdi <m.javaherisaber@gmail.com>
2019-06-27 15:11:15 +0430

    Add 'Live' prefix to Event and EventObserver for better naming

--- core/src/main/java/ir/logicfan/core/ui/reactive/LiveEvent.kt
+++ core/src/main/java/ir/logicfan/core/ui/reactive/LiveEvent.kt
@@ -3,7 +3,7 @@ package ir.logicfan.core.ui.reactive
 /**
  * Used as a wrapper for data that is exposed via a LiveData that represents an event.
  */
-open class Event<out T>(private val content: T) {
+open class LiveEvent<out T>(private val content: T) {
 
     var hasBeenHandled = false
         private set // Allow external read but not write
