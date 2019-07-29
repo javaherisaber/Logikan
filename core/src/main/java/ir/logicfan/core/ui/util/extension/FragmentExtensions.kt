a3c6bcd4ce1ab53ac64d68ec002d6d5b39b300c5
Mahdi <m.javaherisaber@gmail.com>
2019-07-29 19:46:57 +0430

    Add notImplementedToast to FragmentExtensions

--- core/src/main/java/ir/logicfan/core/ui/util/extension/FragmentExtensions.kt
+++ core/src/main/java/ir/logicfan/core/ui/util/extension/FragmentExtensions.kt
@@ -1,5 +1,6 @@
 package ir.logicfan.core.ui.util.extension
 
+import android.widget.Toast
 import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
 
@@ -7,4 +8,8 @@ fun Fragment.openChildFragment(@IdRes viewId: Int, fragment: Fragment) {
     childFragmentManager.beginTransaction()
         .replace(viewId, fragment)
         .commit()
+}
+
+fun Fragment.notImplementedToast() {
+    Toast.makeText(this.context, "Not implemented yet!", Toast.LENGTH_SHORT).show()
 }
