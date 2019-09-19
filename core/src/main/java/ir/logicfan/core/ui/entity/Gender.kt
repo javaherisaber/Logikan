84d235e495f552caf77ad3ec9a1e8edc6ea06893
Mohammad hosein <androidnevisha@gmail.com>
2019-09-19 21:34:44 +0430

    Add localized label to gender and sheba code converter

--- core/src/main/java/ir/logicfan/core/ui/entity/Gender.kt
+++ core/src/main/java/ir/logicfan/core/ui/entity/Gender.kt
@@ -1,9 +1,14 @@
 package ir.logicfan.core.ui.entity
 
-enum class Gender(val code: Int, val label: String, val index: Int) {
-    OTHER(0, "other", -1),
-    MALE(1, "male", 0),
-    FEMALE(2, "female", 1);
+import android.content.Context
+import androidx.annotation.IdRes
+import androidx.annotation.StringRes
+import ir.logicfan.core.R
+
+enum class Gender(val code: Int, val label: String, val index: Int, @StringRes val localizedLabel: Int) {
+    MALE(1, "male", 0, R.string.core_all_male),
+    FEMALE(2, "female", 1, R.string.core_all_female),
+    OTHER(0, "other", -1, R.string.core_all_other);
 
     companion object {
         @JvmStatic
@@ -15,6 +20,36 @@ enum class Gender(val code: Int, val label: String, val index: Int) {
                     // found a gender with given label
                     return item
                 }
+            }
+            throw IllegalArgumentException()
+        }
+
+        @JvmStatic
+        @Throws(IllegalArgumentException::class)
+        fun getGenderWithLocalizedLabel(label: String, context: Context): Gender {
+            val allGenders = values()
+            for (item in allGenders) {
+                if (context.getString(item.localizedLabel) == label) {
+                    // found a gender with given label
+                    return item
+                }
+            }
+            throw IllegalArgumentException()
+        }
+
+        @JvmStatic
+        @Throws(IllegalArgumentException::class)
+        fun getLocalizedLabelWithLabel(label: String?, context: Context): String {
+            val allGenders = values()
+            if (label != null) {
+                for (item in allGenders) {
+                    if (item.label == label) {
+                        // found a gender with given label
+                        return context.getString(item.localizedLabel)
+                    }
+                }
+            } else {
+                return ""
             }
             throw IllegalArgumentException()
         }
@@ -45,4 +80,5 @@ enum class Gender(val code: Int, val label: String, val index: Int) {
             throw IllegalArgumentException()
         }
     }
+
 }
