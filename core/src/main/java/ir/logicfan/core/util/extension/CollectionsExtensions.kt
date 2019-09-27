bc8d108fd47d6695c6bee8a21e6d6c950579b234
Mahdi <m.javaherisaber@gmail.com>
2019-09-27 15:16:51 +0330

    Add generic type to CollectionsExtensions.createCommaSeparatedValues()

--- core/src/main/java/ir/logicfan/core/util/extension/CollectionsExtensions.kt
+++ core/src/main/java/ir/logicfan/core/util/extension/CollectionsExtensions.kt
@@ -4,8 +4,9 @@ package ir.logicfan.core.util.extension
  * Generate comma separated values from your Collection of integer
  * eg. 1,3,5,7,8
  */
-fun List<Int>.createCommaSeparatedValues(): String = this.joinToString(separator = ",") { it.toString() }
-fun Set<Int>.createCommaSeparatedValues(): String = this.joinToString(separator = ",") { it.toString() }
+fun <T> List<T>.createCommaSeparatedValues(): String = this.joinToString(separator = ",") { it.toString() }
+fun <T> Set<T>.createCommaSeparatedValues(): String = this.joinToString(separator = ",") { it.toString() }
+
 fun <T : Any> Collection<T?>.whenAllNotNull(block: () -> Unit) {
     if (this.all { it != null }) {
         block()
