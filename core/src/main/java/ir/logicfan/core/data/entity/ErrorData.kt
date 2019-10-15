9de077a5ed26cdb34b4bba7f6b633b53fa40b85e
Mahdi <m.javaherisaber@gmail.com>
2019-10-15 16:27:16 +0330

    Change `msg` field in Network ErrorData to `message`

--- core/src/main/java/ir/logicfan/core/data/entity/ErrorData.kt
+++ core/src/main/java/ir/logicfan/core/data/entity/ErrorData.kt
@@ -5,6 +5,6 @@ import com.google.gson.annotations.SerializedName
 data class ErrorData(
     @field:SerializedName("code")
     val code: Int,
-    @field:SerializedName("msg")
+    @field:SerializedName("message")
     val msg: String
 )
