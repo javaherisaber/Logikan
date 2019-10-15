289145c3ba513a5dbb53d15f76f8752d830958dc
Mahdi <m.javaherisaber@gmail.com>
2019-10-15 17:58:16 +0330

    Use HasAndroidInjector in BaseActivity & BaseFragment (dagger interface)
    Because we need latest updates of Activity and Fragment libraries

--- core/src/main/java/ir/logicfan/core/ui/base/BaseActivity.kt
+++ core/src/main/java/ir/logicfan/core/ui/base/BaseActivity.kt
@@ -1,7 +1,11 @@
 package ir.logicfan.core.ui.base
 
 import android.os.Bundle
-import dagger.android.support.DaggerAppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
+import dagger.android.AndroidInjection
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
 import ir.logicfan.core.data.sharedpreferences.BaseSharedPreferences
 import ir.logicfan.core.ui.util.LocaleUtils
 import javax.inject.Inject
@@ -11,13 +15,18 @@ import javax.inject.Inject
  * All of our activities extends this class to inherit top level functionality
  */
 
-abstract class BaseActivity : DaggerAppCompatActivity() {
+abstract class BaseActivity : AppCompatActivity(), HasAndroidInjector {
 
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
     @Inject
     lateinit var baseSharedPreferences: BaseSharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         LocaleUtils.setLocale(this, baseSharedPreferences.localeSetting)
     }
+
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 }
--- core/src/main/java/ir/logicfan/core/ui/base/BaseFragment.kt
+++ core/src/main/java/ir/logicfan/core/ui/base/BaseFragment.kt
@@ -1,19 +1,35 @@
 package ir.logicfan.core.ui.base
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.annotation.CallSuper
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import dagger.android.support.DaggerFragment
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasAndroidInjector
+import dagger.android.support.AndroidSupportInjection
 import ir.logicfan.core.ui.util.delegate.autoClearedActivityListener
+import javax.inject.Inject
 
-abstract class BaseFragment : DaggerFragment() {
+abstract class BaseFragment : Fragment(), HasAndroidInjector {
 
     private val dataTerminalErrorListener by autoClearedActivityListener<DataTerminalErrorListener>()
 
+    @Inject
+    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
     abstract fun attachBaseViewModel(): BaseViewModel
+
+    override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
+        super.onAttach(context)
+    }
+
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 
     @CallSuper // a standard annotation from AndroidX library, forcing derived function to call super
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
