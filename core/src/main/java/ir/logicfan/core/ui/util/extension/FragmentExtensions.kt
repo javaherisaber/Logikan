938bfe754bd89bb0b4e23f7ca3c2bb2d38761e34
Mahdi <m.javaherisaber@gmail.com>
2019-10-17 18:58:18 +0330

    Add toolbar extension functions to FragmentExtensions

--- core/src/main/java/ir/logicfan/core/ui/util/extension/FragmentExtensions.kt
+++ core/src/main/java/ir/logicfan/core/ui/util/extension/FragmentExtensions.kt
@@ -2,8 +2,22 @@ package ir.logicfan.core.ui.util.extension
 
 import android.widget.Toast
 import androidx.annotation.IdRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import ir.logicfan.core.R
+
+fun Fragment.initializeToolbar(toolbar: Toolbar, displayTitle: Boolean = false, displayBack: Boolean = true) =
+    (this.activity as AppCompatActivity).apply {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(displayTitle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(displayBack)
+    }
+
+fun Fragment.setToolbarTitle(title: String) = (this.activity as AppCompatActivity).supportActionBar?.apply {
+    setDisplayShowTitleEnabled(true)
+    this.title = title
+}
 
 fun Fragment.openChildFragment(@IdRes viewId: Int, fragment: Fragment, addToBackStack: Boolean = false) {
     childFragmentManager.beginTransaction().apply {
