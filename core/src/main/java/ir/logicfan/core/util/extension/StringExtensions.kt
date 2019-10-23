f3574439816738fafcb5b37c9dca1e0db15555d5
Mahdi <m.javaherisaber@gmail.com>
2019-10-23 16:27:49 +0330

    Add nullable support to String.stripHtml extension function

--- core/src/main/java/ir/logicfan/core/util/extension/StringExtensions.kt
+++ core/src/main/java/ir/logicfan/core/util/extension/StringExtensions.kt
@@ -4,11 +4,11 @@ import android.os.Build
 import android.text.Html
 import android.text.Spanned
 
-fun String.stripHtml(): Spanned =
+fun String?.stripHtml(): Spanned =
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-        Html.fromHtml(this, Html.FROM_HTML_MODE_COMPACT)
+        Html.fromHtml(this ?: "", Html.FROM_HTML_MODE_COMPACT)
     } else @Suppress("DEPRECATION") {
-        Html.fromHtml(this)
+        Html.fromHtml(this ?: "")
     }
 
 fun String.isValidIranMobile(): Boolean = this.matches("(0(9[0-9]{9}))|(\\+98(9[0-9]{9}))".toRegex())
