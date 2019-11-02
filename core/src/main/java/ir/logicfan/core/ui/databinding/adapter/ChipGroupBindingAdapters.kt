6200b1fb77825f27a4da8e6a47b8ed54faea00c9
Mahdi <m.javaherisaber@gmail.com>
2019-11-02 17:25:23 +0330

    Add inflateEntryChip extension function to ChipGroupBindingAdapters

--- core/src/main/java/ir/logicfan/core/ui/databinding/adapter/ChipGroupBindingAdapters.kt
+++ core/src/main/java/ir/logicfan/core/ui/databinding/adapter/ChipGroupBindingAdapters.kt
@@ -26,24 +26,33 @@ fun ChipGroup.addDecorationChips(data: Collection<String>?) = data?.let {
 /**
  * Inflate Chip with entry style into ChipGroup
  */
-@BindingAdapter(value = ["entryChipsData", "onCloseIconClick"], requireAll = false)
+@BindingAdapter(value = ["entryChipsData", "onCloseIconClick", "keepChildren"], requireAll = false)
 fun ChipGroup.addEntryChips(
-    data: Collection<Pair<Int, String>>?, listener: OnChipCloseIconClickListener?
+    data: Collection<Pair<Int, String>>?, listener: OnChipCloseIconClickListener?, keepChildren: Boolean?
 ) = data?.let {
-    if (this.childCount > 0) {
+    if (this.childCount > 0 && (keepChildren == false || keepChildren == null)) {
         removeAllViews()
     }
     val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
     it.forEach { item ->
-        val chip = inflater.inflate(R.layout.core_view_chip_entry, this, false) as Chip
-        chip.id = item.first
-        chip.text = item.second
-        chip.setOnCloseIconClickListener { view ->
-            this.removeView(view)
-            listener?.onCloseIconClick(item.first, item.second)
-        }
+        val chip = this.inflateEntryChip(inflater, item, listener)
         this.addView(chip)
     }
+}
+
+fun ChipGroup.inflateEntryChip(
+    inflater: LayoutInflater,
+    item: Pair<Int, String>,
+    listener: OnChipCloseIconClickListener?
+): Chip {
+    val chip = inflater.inflate(R.layout.core_view_chip_entry, this, false) as Chip
+    chip.id = item.first
+    chip.text = item.second
+    chip.setOnCloseIconClickListener { view ->
+        this.removeView(view)
+        listener?.onCloseIconClick(item.first, item.second)
+    }
+    return chip
 }
 
 interface OnChipCloseIconClickListener {
