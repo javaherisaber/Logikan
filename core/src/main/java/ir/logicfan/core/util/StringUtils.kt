4cda338f14931c326f6472c523224b412fa26bb2
Mahdi <m.javaherisaber@gmail.com>
2020-02-19 19:20:13 +0330

    Move toSeparatedThousands() from StringUtils to NumberExtensions

--- core/src/main/java/ir/logicfan/core/util/StringUtils.kt
+++ core/src/main/java/ir/logicfan/core/util/StringUtils.kt
@@ -2,6 +2,10 @@ package ir.logicfan.core.util
 
 object StringUtils {
 
+    @Deprecated("It is ineffective function", ReplaceWith(
+        "number.toSeparatedThousands()",
+        "ir.logicfan.core.util.extension.toSeparatedThousands"
+    ))
     @JvmStatic
     fun numberToSeparatedThousands(number: Long): String = String.format("%,d", number)
 }
--- core/src/main/java/ir/logicfan/core/util/extension/IntegerExtensions.kt
+++
@@ -1,6 +0,0 @@
-package ir.logicfan.core.util.extension
-
-/**
- * @return number with zero tail if less than 10 -> eg. 07
- */
-fun Int.toZeroTail(): String = if (this < 10) "0$this" else this.toString()
