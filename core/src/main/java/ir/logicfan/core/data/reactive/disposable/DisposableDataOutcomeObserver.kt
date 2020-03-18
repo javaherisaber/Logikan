c044c1819db4651fb03101b0e8bc51d6d8987bc1
Mahdi <m.javaherisaber@gmail.com>
2020-03-18 21:11:34 +0330

    Add copyright signature to all files

--- core/src/main/java/ir/logicfan/core/data/reactive/disposable/DisposableDataOutcomeObserver.kt
+++ core/src/main/java/ir/logicfan/core/data/reactive/disposable/DisposableDataOutcomeObserver.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) Logicfan, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited Proprietary and confidential
+ * Written by Mahdi Javaheri Saber <m.javaherisaber@gmail.com>, Mohammad Hosein Abedini <mohammadhoseinabedini@gmail.com>  2020.
+ */
+
 package ir.logicfan.core.data.reactive.disposable
 
 import ir.logicfan.core.data.base.DataOutcome
