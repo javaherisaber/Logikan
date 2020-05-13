6ce988122b4aa81404252df2b953c0d08f69b7b7
Mahdi <m.javaherisaber@gmail.com>
2020-05-13 11:35:16 +0430

    Remove Licence from single files and use module LICENCE instead

--- core/src/main/java/ir/logicfan/core/ui/util/GraphicUtils.kt
+++ core/src/main/java/ir/logicfan/core/ui/util/GraphicUtils.kt
@@ -1,9 +1,3 @@
-/*
- * Copyright (C) Logicfan, Inc - All Rights Reserved
- * Unauthorized copying of this file, via any medium is strictly prohibited Proprietary and confidential
- * Written by Mahdi Javaheri Saber <m.javaherisaber@gmail.com>, Mohammad Hosein Abedini <mohammadhoseinabedini@gmail.com>  2020.
- */
-
 package ir.logicfan.core.ui.util
 
 import android.graphics.*
