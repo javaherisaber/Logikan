7a97e1f6a6f616647ae0f2d97ab9ad32402a2c2b
Mahdi Javaheri <m.javaherisaber@gmail.com>
2020-06-29 16:57:39 +0430

    Create typealias for OneShotEvent

--- core/src/main/java/ir/logicfan/core/ui/reactive/OneShotEvent.kt
+++ core/src/main/java/ir/logicfan/core/ui/reactive/OneShotEvent.kt
@@ -1,8 +1,16 @@
 package ir.logicfan.core.ui.reactive
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
 /**
  * Used as a wrapper for data that is exposed via a LiveData that represents an event.
  */
+typealias MutableOneShotLiveEvent<T> = MutableLiveData<OneShotEvent<T>>
+typealias OneShotLiveEvent<T> = LiveData<OneShotEvent<T>>
+typealias MutableOneShotUnitLiveEvent = MutableOneShotLiveEvent<Unit>
+typealias OneShotUnitLiveEvent = OneShotLiveEvent<Unit>
+
 open class OneShotEvent<out T>(private val content: T) {
 
     var hasBeenHandled = false
