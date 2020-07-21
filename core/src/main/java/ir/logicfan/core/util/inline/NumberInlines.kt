aa7cb42dbb24b7fd4414f271f533eed6cbe3cc80
Mahdi Javaheri <m.javaherisaber@gmail.com>
2020-07-21 10:11:31 +0430

    Fix whenNotNullOrZero bug

--- core/src/main/java/ir/logicfan/core/util/inline/NumberInlines.kt
+++ core/src/main/java/ir/logicfan/core/util/inline/NumberInlines.kt
@@ -3,8 +3,8 @@ package ir.logicfan.core.util.inline
 /**
  * invoke [callback] when [input] is not null or zero
  */
-inline fun <T, R> whenNotNullOrZero(input: T?, callback: (T) -> R): R? {
-    return if (input != null && input != 0) {
+inline fun <T: Number, R> whenNotNullOrZero(input: T?, callback: (T) -> R): R? {
+    return if (input != null && input.toDouble() != 0.0) {
         callback.invoke(input)
     } else null
 }
