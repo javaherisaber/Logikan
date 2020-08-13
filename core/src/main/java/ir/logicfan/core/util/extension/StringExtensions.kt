a7aae732a15a33f3652d6ce2cfb350ee5238d362
Mahdi Javaheri <m.javaheri@bazaarshahr.ir>
2020-08-13 09:18:23 +0430

    Create String.mimeTypeForUrl extension function

--- core/src/main/java/ir/logicfan/core/util/extension/StringExtensions.kt
+++ core/src/main/java/ir/logicfan/core/util/extension/StringExtensions.kt
@@ -3,6 +3,7 @@ package ir.logicfan.core.util.extension
 import android.os.Build
 import android.text.Html
 import android.text.Spanned
+import android.webkit.MimeTypeMap
 
 fun String?.stripHtml(): Spanned = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
     Html.fromHtml(this ?: "", Html.FROM_HTML_MODE_COMPACT)
@@ -26,3 +27,15 @@ fun String.toBearerToken(): String = if (this != "null") "Bearer $this" else thi
  * Remove ',' chars from text
  */
 fun String.stripThousandsSeparator(): String = this.replace(",", "")
+
+/**
+ * Get mime type for a given url
+ */
+fun String.mimeTypeForUrl(): String? {
+    var type: String? = null
+    val extension = MimeTypeMap.getFileExtensionFromUrl(this)
+    if (extension != null) {
+        type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
+    }
+    return type
+}
