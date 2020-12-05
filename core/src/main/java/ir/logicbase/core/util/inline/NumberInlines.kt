8f33d53ed4333f2afd4c4e761773bad079af1364
Mahdi Javaheri <m.javaherisaber@gmail.com>
2020-12-05 12:42:02 +0330

    Move sumByLong() function to IterableExtensions.kt

--- core/src/main/java/ir/logicbase/core/util/inline/NumberInlines.kt
+++ core/src/main/java/ir/logicbase/core/util/inline/NumberInlines.kt
@@ -9,13 +9,8 @@ inline fun <T: Number, R> whenNotNullOrZero(input: T?, callback: (T) -> R): R? {
     } else null
 }
 
-/**
- * Returns the sum of all values produced by [selector] function applied to each element in the collection.
- */
-inline fun <T> Iterable<T>.sumByLong(selector: (T) -> Long): Long {
-    var sum = 0L
-    for (element in this) {
-        sum += selector(element)
-    }
-    return sum
+inline fun <T: Number, R> whenNullOrZero(input: T?, callback: (T?) -> R): R? {
+    return if (input == null || input.toDouble() == 0.0) {
+        callback.invoke(input)
+    } else null
 }
